c8bd2e222000870b8a5381c69e9958db54af14f0
anthony1500 <arteaga17000@gmail.com>
2024-02-19 08:45:57 -0500

    2.0

--- logout.php
+++ logout.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * Este archivo inicia y luego destruye una sesión.
+ * También elimina una cookie establecida y luego redirige al usuario a 'index.php'.
+ *
+ * PHP version 8
+ *
+ * @category Session_Management
+ * @package  Session_Package
+ * @author   Your Name <your.email@example.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://www.itsbolivar.edu.ec/web/index.php
+ */
+
 session_start(); // Inicia la sesión
 session_destroy(); // Destruye la sesión
+
 // Establece la cookie con una fecha de vencimiento en el pasado para eliminarla
 setcookie('modalShown', '', time() - 3600);
+
 header('Location: index.php'); // Redirige al usuario a 'index.php'
-?>
+
